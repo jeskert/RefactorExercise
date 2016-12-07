3c09e8db6c183fc6c3fd47e2dbff6586ce3c93ed
jeskert <568361463@qq.com>
2016-12-07 14:57:07 +0800

    Refactor 1

---
+++ src/Rental.java
@@ -0,0 +1,21 @@
+/**
+ * Created by wyli on 16/12/7.
+ */
+public class Rental {
+
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        _movie = movie;
+        _daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return _movie;
+    }
+
+    public int getDaysRented() {
+        return _daysRented;
+    }
+}
---
+++ src/test/CustomerTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+    @Test
+    public void statement() throws Exception {
+        Customer customer = dummyCustomer();
+        assertEquals(customer.statement(), "Rental Records for jeskert\n" +
+                "\tTitanic\t0.0\n" +
+                "Amount owed is 0.0\n" +
+                "You earned 1 frequent renter points");
+    }
+
+    private Customer dummyCustomer() {
+        Customer customer = new Customer("jeskert");
+        Movie movie = new Movie("Titanic", 20);
+        Rental rental = new Rental(movie, 5);
+        customer.addRental(rental);
+        return customer;
+    }
+}
